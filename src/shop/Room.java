dd51df770908bbd2ad00717b59e91efa918bc4a6
PhForty <37180056+PhForty@users.noreply.github.com>
2018-03-09 10:45:51 +0100

    Add files via upload

---
+++ src/shop/Room.java
@@ -0,0 +1,102 @@
+package shop;
+
+/**
+ * abstract motherclass for show- and storageroom 
+ * contains an amount- and max array with its corresponding getter and setter Methods
+ * and methods to increase and decrease amount, which is needed in every room
+ * 
+ * @author Philipp Fortmann
+ *
+ */
+public abstract class Room {
+	
+    private int[] amount = new int[6];
+    private int[] max = new int[6];
+    String[] beverages =  {"Mineralwasser (still)","Mineralwasser (mit K.)","Apfelsaft","Orangensaft","Limonade","Bier"};
+    
+    /**
+     * gets the amount of a specified drink and returns it
+     * 
+     * @param drink drink from which the amount is given back
+     * @return returns the amount of the specified drink. returns -1 if drink does not exist
+     */
+    public int getAmount(int drink) {
+    	//check if drink exists
+    	if(drink>=0&&drink<6) {
+    		return amount[drink];
+    	} else {
+    		return -1;
+    	}
+    }
+    /**
+     * sets the amount of a given drink to a given value
+     * 
+     * @param drink drink whose amount is to be changed
+     * @param value value which will be the new amount 
+     */
+    public void setAmount(int drink, int value) {
+    	amount[drink] = value;
+    }
+    
+    /**
+     * increases the amount of a given drink by a given value,
+     * if there is not enough space left the remaining drinks will be ignored
+     * 
+     * @param drink drink whose amount is to be increased
+     * @param inc value by which the amount is increased
+     */
+    public void increaseAmount(int drink, int inc) {
+    	//checks if amount can be increased by the full inc value
+    	//if not: amount is set to max, the remaining drinks are dropped
+    	if(getAmount(drink)>(getMax(drink)-inc)) {
+    		System.out.println("Fehler: Es war nur Platz für "+ (getMax(drink)-getAmount(drink)) +" Kästen. Das Lager ist jetzt voll");
+    		setAmount(drink, getMax(drink));
+    	} else {
+    		setAmount(drink, getAmount(drink) + inc);
+    	}
+    }
+
+    /**
+     * decreases the amount of a given drink by a given value, if possible
+     * 
+     * @param drink drink whose amount is to be decreased
+     * @param dec value by which the amount is decreased (expects positive Integer)
+     */
+    public void decreaseAmount(int drink, int dec) {
+    	//checks if amount would get negative. If thats the case nothing is decreased
+    	if(getAmount(drink)<dec) {
+    		System.out.println("Fehler: Es sollen mehr Kästen weggenommen werden als da sind. Es wurde nichts verändert.");
+    	} else {
+    		setAmount(drink, getAmount(drink) - dec);
+    	}
+    }
+    
+    /**
+     * returns the max of a given drink
+     * 
+     * @param drink specifies which drink's max is wanted
+     * @return the max value of the given drink, returns -1 if drink does not exist
+     */
+    public int getMax(int drink) {
+    	//check if drink exists
+    	if(drink>=0&&drink<6) {
+    		return max[drink];
+    	} else {
+    		return -1;
+    	}
+    }
+    
+    /**
+     * sets max of a given drink to a given value
+     * 
+     * @param drink specifies which drink's max is to be newly set
+     * @param value the value by to which max is set
+     */
+    public void setMax(int drink, int value) {
+    	max[drink] = value;
+    }
+    
+
+
+
+}
---
+++ src/shop/Showroom.java
@@ -0,0 +1,97 @@
+package shop;
+
+/**
+ * This class represents the show room
+ * 
+ * @author Philipp Fortmann
+ *
+ */
+public class Showroom extends Room {
+	
+	/**
+	 * constructor creates showroom with specific values for max and amount
+	 */
+    public Showroom() {
+    	
+	    setAmount(0,15);
+	    setMax(0,15);
+	    setAmount(1,15);
+	    setMax(1,15);
+	    setAmount(2,10);
+	    setMax(2,10);
+	    setAmount(3,5);
+	    setMax(3,5);
+	    setAmount(4,10);
+	    setMax(4,10);
+	    setAmount(5,5);
+	    setMax(5,5);
+	    
+    }
+   
+    /**
+     * this method decreases the amount of a specified drink (if possible)
+     * 
+     * @param drink specifies for which drink the operation is done
+     * @param quantity amount, which shall be bought (and therefore is subtracted)
+     */
+    public void buydrink(int drink, int quantity) {
+		// check if drink exists
+		if (drink >= 0 && drink < 6) {
+			// check if showroom has enough left
+			if (this.getAmount(drink) >= quantity) {
+				this.decreaseAmount(drink, quantity);
+				System.out.println(quantity + " Kästen wurden erfolgreich verkauft.");
+			} else {
+				System.out.println("Fehler. Im Verkaufsraum gibt es nur " + this.getAmount(drink) + " Kästen");
+			}
+		}
+	}
+    
+    /**
+     * calculates where the amount and max do not match in the show room
+     * then transfers the missing amounts from the chosen storage room to the show room
+     * 
+     * @param roomnumber the room from where the show room will be filled
+     * @param storer1 storage room 1, to apply the possible changes
+     * @param storer2 storage room 2, to apply the possible changes
+     */
+    public void fillShowRoom(int roomnumber, Storageroom storer1, Storageroom storer2) {
+		//checks if roomnumber is valid
+		if(roomnumber==1||roomnumber==2) {
+			//iterates for all drinks
+			for(int i = 0; i<6; i++) {
+				int difference = this.getMax(i) - this.getAmount(i);
+					//if room 1 was chosen:
+					if(roomnumber==1) {
+						//if there is enough left in storageroom:
+						if(storer1.getAmount(i)>=difference) {
+							storer1.decreaseAmount(i, difference);
+							this.increaseAmount(i, difference);
+						} 
+						//if there is not enough left in storageroom:
+						else {
+							this.increaseAmount(i, storer1.getAmount(i));
+							storer1.setAmount(i, 0);
+						}
+					} 
+					//if room 2 was chosen:
+					else {
+						//if there is enough left in storageroom:
+						if(storer2.getAmount(i)>=difference) {
+							storer2.decreaseAmount(i, difference);
+							this.increaseAmount(i, difference);
+						} 
+						//if there is not enough left in storageroom:
+						else {
+							this.increaseAmount(i, storer2.getAmount(i));
+							storer2.setAmount(i, 0);
+						}
+					}
+				}
+			System.out.println("Die Getränke im Verkaufsraum wurden, soweit möglich, aus dem Lager "+roomnumber+" wieder aufgestockt");
+		} else {
+			System.out.println("Diese Raumnummer gibt es nicht. Bitte wählen sie '1' oder '2'");
+		}
+	}
+
+}
